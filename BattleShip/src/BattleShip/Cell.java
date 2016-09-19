8c4b49c7a845e818ef1fb28915c51de1a0635768
Tiffany Phan <tiffany.phan@afit.edu>
2016-09-19 08:01:19 -0400

    added BattleShip java

---
+++ BattleShip/src/BattleShip/Cell.java
@@ -0,0 +1,46 @@
+package BattleShip;
+
+public class Cell
+{
+	protected boolean struckByMissile = false;
+	protected Ship ship = null;
+	
+	public Cell()
+	{		
+	}
+	
+	public boolean hasBeenStruckByMissile()
+	{
+		if (this.ship.isAlive()) {
+			this.struckByMissile = true;
+		} 
+		return this.struckByMissile;
+	}
+	
+	public void hasBeenStruckByMissile( boolean wasStruck )
+	{	
+		if (wasStruck) {
+			this.struckByMissile = true;
+		}
+	}
+	
+	public char draw()
+	{
+		if( this.ship == null )
+		{
+			if( this.struckByMissile )
+				return 'x';
+			return ' ';
+		}
+		//a ship is at this cell
+		return ship.drawShipStatusAtCell( this.struckByMissile );			
+	}
+	
+	public Ship getShip() { return this.ship; }
+	public void setShip( Ship s ) { this.ship = s; }
+
+	public static void main(String[] args)
+	{
+	}
+
+}
